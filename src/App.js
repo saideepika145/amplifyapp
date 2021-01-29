f2941fd92ae6654322d79e7c468057f648c21e44
Areti Sai Deepika <58738115+saideepika145@users.noreply.github.com>
2021-01-29 12:30:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Welcome to CCIOT SKILL</h1>
-        <h2>Sridevi</h2>
+        <h2>Navya</h2>
       </header>
     </div>
   );
